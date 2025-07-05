897f707b314c603cd068d7eb621de3e1aa0c7e3a
Mehdi Salimi <88512274+SalimiDev@users.noreply.github.com>
2025-07-05 23:20:10 +0330

    chore: Enhance next-intl files and structure (#10)

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,28 @@
 import "../globals.css";
 import { NextIntlClientProvider } from "next-intl";
-import { getMessages, setRequestLocale } from "next-intl/server";
+import { getMessages, getTranslations, setRequestLocale } from "next-intl/server";
 import { AppLocale, locales } from "../../lib/i18n/config";
 import { notFound } from "next/navigation";
+import { routing } from "../../lib/i18n/routing";
 
 type Props = {
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 };
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+export async function generateMetadata(props: Omit<Props, "children">) {
+  const { locale } = await props.params;
+  const t = await getTranslations({ locale, namespace: "LocaleLayout" });
+
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
 
 export default async function LocaleLayout({ children, params }: Props) {
   const { locale } = await params;
@@ -28,7 +43,7 @@ export default async function LocaleLayout({ children, params }: Props) {
     >
       <body suppressHydrationWarning>
         <NextIntlClientProvider locale={locale} messages={messages}>
-          {children}
+          <main>{children}</main>
         </NextIntlClientProvider>
       </body>
     </html>
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -14,6 +14,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-[calc(100vh-64px)]">
       <h1>{t("title")}</h1>
+      <p>{t("description")}</p>
     </div>
   );
 }
