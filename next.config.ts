33f31884e42f2abd2a6982b589d4f09d33205002
Mehdi Salimi <Salimi.devop@gmail.com>
2025-07-18 23:21:48 +0330

    chore: configure tsconfig paths and eslint import resolver for aliasing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+import path from "path";
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
-
 const withNextIntl = createNextIntlPlugin("./src/lib/i18n/request.ts");
+
+const nextConfig: NextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
+};
+
 export default withNextIntl(nextConfig);
